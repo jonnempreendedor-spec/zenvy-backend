fdb19b95b8d1d8da3a619e858410c4f6485b2960
jonnempreendedor-spec <jonnempreendedor@gmail.com>
2026-05-01 12:46:18 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Zenvy API online 🚀");
+});
+
+app.post("/ai", (req, res) => {
+  const { message } = req.body;
+
+  res.json({
+    reply: "IA respondeu: " + message
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
