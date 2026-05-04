0732071ceb75c9340123c01b63fea4358fc4b763
John <jonnempreendedor@gmail.com>
2026-05-04 20:44:31 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import whatsappRoutes from "./src/routes/whatsapp.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", whatsappRoutes);
+
+app.get("/", (req, res) => {
+  res.send("API rodando 🚀");
+});
+
+app.listen(3000, () => {
+  console.log("Servidor ativo");
+});
